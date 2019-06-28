9725f957054bb203a23c1a27c6ed28ab2ebbb20a
hsd <xiivixxiv520@foxmail.com>
2019-06-28 09:45:36 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # apscheduler_template
 apscheduler 任务调度框架模板
+BlockingScheduler 下 cron、interval、date 模块的简单运用
---
+++ apscheduler_temp.py
@@ -0,0 +1,52 @@
+import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+scheduler = BlockingScheduler()
+
+
+def main_func():
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+
+# ========================= cron定时调度（某一定时时刻执行）=================================== #
+"""
+    (int|str) 表示参数既可以是int类型，也可以是str类型
+    (datetime | str) 表示参数既可以是datetime类型，也可以是str类型
+    year (int|str) – 4-digit year -（表示四位数的年份，如2008年）
+    month (int|str) – month (1-12) -（表示取值范围为1-12月）
+    day (int|str) – day of the (1-31) -（表示取值范围为1-31日）
+    week (int|str) – ISO week (1-53) -（格里历2006年12月31日可以写成2006年-W52-7（扩展形式）或2006W527（紧凑形式））
+    day_of_week (int|str) – number or name of weekday (0-6 or mon,tue,wed,thu,fri,sat,sun) - （表示一周中的第几天，既可以用0-6表示也可以用其英语缩写表示）
+    hour (int|str) – hour (0-23) - （表示取值范围为0-23时）
+    minute (int|str) – minute (0-59) - （表示取值范围为0-59分）
+    second (int|str) – second (0-59) - （表示取值范围为0-59秒）
+    start_date (datetime|str) – earliest possible date/time to trigger on (inclusive) - （表示开始时间）
+    end_date (datetime|str) – latest possible date/time to trigger on (inclusive) - （表示结束时间）
+    timezone (datetime.tzinfo|str) – time zone to use for the date/time calculations (defaults to scheduler timezone) -（表示时区取值）
+"""
+#表示2017年3月22日17时19分07秒执行该程序
+scheduler.add_job(main_func, 'cron', year=2017, month=3, day=22, hour=17, minute=19, second=7)
+
+# ============================ interval 间隔调度（每隔多久执行）================================ #
+"""
+    weeks (int) – number of weeks to wait
+    days (int) – number of days to wait
+    hours (int) – number of hours to wait
+    minutes (int) – number of minutes to wait
+    seconds (int) – number of seconds to wait
+    start_date (datetime|str) – starting point for the interval calculation
+    end_date (datetime|str) – latest possible date/time to trigger on
+    timezone (datetime.tzinfo|str) – time zone to use for the date/time calculations
+"""
+#表示每隔3天17时19分07秒执行一次任务
+scheduler.add_job(main_func, 'interval', days=3, hours=17, minutes=19, seconds=7)
+
+# ============================== date 定时调度（作业只会执行一次）============================== #
+"""
+    run_date (datetime|str) – the date/time to run the job at  -（任务开始的时间）
+    timezone (datetime.tzinfo|str) – time zone for run_date if it doesn’t have one already
+"""
+# The job will be executed on November 6th, 2009 at 16:30:05
+scheduler.add_job(main_func, 'date', run_date=datetime.datetime(2009, 11, 6, 16, 30, 5), args=['text'])
+
+scheduler.start()
